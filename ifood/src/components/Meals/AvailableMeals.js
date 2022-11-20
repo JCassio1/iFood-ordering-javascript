0282618ed8657a7802ddff8c4cad4c13dab3799c
JCassio1 <Joselsondias@hotmail.com>
2022-11-20 20:45:58 +0000

    Add boostrap card component

--- ifood/src/components/Meals/AvailableMeals.js
+++ ifood/src/components/Meals/AvailableMeals.js
@@ -1,36 +1,55 @@
+import UICard from '../UI/UICard'
+import styles from './AvailableMeals.module.css'
+
 const DUMMY_MEALS = [
   {
     id: 'm1',
     name: 'Sushi',
     description: 'Finest fish and veggies',
+    imagePath:
+      'https://peasandcrayons.com/wp-content/uploads/2014/05/southwest-fusion-yellow-rice-sushi-with-roasted-veggies-and-shrimp-450.jpg',
     price: 22.99
   },
   {
     id: 'm2',
     name: 'Schnitzel',
     description: 'A german specialty!',
+    imagePath: 'https://bestellen.clubleen.nl/391-medium_default/schnitzel.jpg',
     price: 16.5
   },
   {
     id: 'm3',
     name: 'Barbecue Burger',
     description: 'American, raw, meaty',
+    imagePath:
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https://static.onecms.io/wp-content/uploads/sites/19/2013/06/03/tennessee-burger-bourbon-bbq-sauce-ck-x.jpg',
     price: 12.99
   },
   {
     id: 'm4',
     name: 'Green Bowl',
     description: 'Healthy...and green...',
+    imagePath: 'https://i.pinimg.com/736x/2f/76/5b/2f765b02113f2e9d92548f8ee589f468.jpg',
     price: 18.99
   }
 ]
 
 const AvailableMeals = () => {
-  const listOfMeals = DUMMY_MEALS.map((meal) => <li key={meal.name}>{meal.name}</li>)
-
+  const generateCard = (meal) => {
+    return (
+      <UICard
+        key={meal.id}
+        title={meal.name}
+        description={meal.description}
+        buttonText={meal.price}
+        imagePath={meal.imagePath}
+      />
+    )
+  }
+  const listOfMeals = DUMMY_MEALS.map(generateCard)
   return (
-    <section>
-      <ul>{listOfMeals}</ul>
+    <section className='bg-light pb-4'>
+      <div className={`row px-4 ${styles.spaceItemsEvenly}`}>{listOfMeals}</div>
     </section>
   )
 }
---
+++ ifood/src/components/UI/UICard.js
@@ -0,0 +1,22 @@
+import { Fragment } from 'react'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+
+const UICard = (props) => {
+  return (
+    <Fragment>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant='top' src={props.imagePath} />
+        <Card.Body>
+          <Card.Title className='text-center'>{props.title}</Card.Title>
+          <Card.Text className='text-center'>{props.description}</Card.Text>
+          <div className='text-center'>
+            <Button variant='primary'>£ {props.buttonText}</Button>
+          </div>
+        </Card.Body>
+      </Card>
+    </Fragment>
+  )
+}
+
+export default UICard
