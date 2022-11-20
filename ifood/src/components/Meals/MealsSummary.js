1ef8b6050ef0c132fd1221eff2b73e08bfcbe403
JCassio1 <Joselsondias@hotmail.com>
2022-11-20 18:35:04 +0000

    Create information about meals

---
+++ ifood/src/components/Meals/MealsSummary.js
@@ -0,0 +1,14 @@
+const MealsSummary = () => {
+  return (
+    <section>
+      <div className='p-5 text-center bg-light'>
+        <h2>One app, all things food related right at your doorstep</h2>
+        <p>
+          Choose your favourite meal from our broad selection across different cuisines and enjoy it at home at home
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default MealsSummary
