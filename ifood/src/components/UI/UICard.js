5246bf5685b386523211eaf18d3d616230f263fc
JCassio1 <Joselsondias@hotmail.com>
2022-11-21 11:16:42 +0000

    Add hover effect

--- ifood/src/components/UI/UICard.js
+++ ifood/src/components/UI/UICard.js
@@ -1,15 +1,16 @@
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
+import styles from './UICard.module.css'
 
 const UICard = (props) => {
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card className={styles.card}>
       <Card.Img variant='top' src={props.imagePath} />
       <Card.Body>
         <Card.Title className='text-center'>{props.title}</Card.Title>
         <Card.Text className='text-center'>{props.description}</Card.Text>
         <div className='text-center'>
-          <Button variant='primary'>£ {props.buttonText}</Button>
+          <Button variant='dark'>£ {props.buttonText}</Button>
         </div>
       </Card.Body>
     </Card>
