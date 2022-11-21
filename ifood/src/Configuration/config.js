28e5b23e29eafe804ead9f3a306eb7b01fb0d5f6
JCassio1 <Joselsondias@hotmail.com>
2022-11-21 15:14:37 +0000

    Add a stepper to add quantity

--- ifood/src/Configuration/config.js
+++ ifood/src/Configuration/config.js
@@ -1,2 +1,3 @@
 export const CAROUSEL_SLIDER_TIME = 3000
 export const MAIN_BACKGROUND_COLOR = 'bg-light'
+export const MAX_ORDERS_PER_PRODUCT = 50
--- ifood/src/components/UI/UICard.js
+++ ifood/src/components/UI/UICard.js
@@ -1,16 +1,43 @@
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import styles from './UICard.module.css'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import { PlusSquareFill, DashSquareFill } from 'react-bootstrap-icons'
+import { useState } from 'react'
+import { MAX_ORDERS_PER_PRODUCT } from '../../Configuration/config'
 
 const UICard = (props) => {
+  const [quantity, setQuantity] = useState(1)
+  const [price, setPrice] = useState(props.buttonText)
+  const increaseQty = () => {
+    if (quantity < MAX_ORDERS_PER_PRODUCT) {
+      setQuantity(quantity + 1)
+    }
+  }
+  const decreaseQty = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1)
+    }
+  }
+
   return (
     <Card className={styles.card}>
       <Card.Img variant='top' src={props.imagePath} />
       <Card.Body className='d-flex flex-column'>
         <Card.Title className='text-center'>{props.title}</Card.Title>
         <Card.Text className='text-center'>{props.description}</Card.Text>
+        <Row className='mt-auto pb-3'>
+          <Col className='d-flex justify-content-center'>
+            <PlusSquareFill onClick={increaseQty} />
+          </Col>
+          <Col className='d-flex justify-content-center'>{quantity}</Col>
+          <Col className='d-flex justify-content-center'>
+            <DashSquareFill onClick={decreaseQty} />
+          </Col>
+        </Row>
         <Button variant='dark' className='mt-auto'>
-          £ {props.buttonText} • Add
+          £ {price} • Add
         </Button>
       </Card.Body>
     </Card>
