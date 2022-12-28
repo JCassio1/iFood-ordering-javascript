87772153f56815303bf42fb8f102664c3eea434f
JCassio1 <Joselsondias@hotmail.com>
2022-12-28 09:08:37 +0100

    TEST ENV

--- ifood/src/App.js
+++ ifood/src/App.js
@@ -12,14 +12,17 @@ function App() {
   const hideCartHandler = () => setShowCartModal(false)
   const showCartHandler = () => setShowCartModal(true)
 
+  console.log(process.env.NAME)
+
   return (
-    <CartProvider>
-      <CartModal showStatus={showCartModal} dissmissHandler={hideCartHandler} />
-      <Header onShowCart={showCartHandler}></Header>
-      <main className={MAIN_BACKGROUND_COLOR}>
-        <Meals />
-      </main>
-    </CartProvider>
+    <h1>HI</h1>
+    // <CartProvider>
+    //   <CartModal showStatus={showCartModal} dissmissHandler={hideCartHandler} />
+    //   <Header onShowCart={showCartHandler}></Header>
+    //   <main className={MAIN_BACKGROUND_COLOR}>
+    //     <Meals />
+    //   </main>
+    // </CartProvider>
   )
 }
 
